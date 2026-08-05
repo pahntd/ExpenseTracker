0a092c1bb30f539bbef15c79e7fcebe4e405e91d
pahntd <pdtrong2652000@gmail.com>
2026-08-05 16:12:38 +0700

    Implement splash screen

--- app/src/main/java/com/example/expensetracker/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/splash/SplashFragment.kt
@@ -6,25 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.expensetracker.R
+import com.example.expensetracker.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.button).setOnClickListener {
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(1000)
             findNavController().navigate(
-                R.id.action_splashFragment_to_homeFragment
+                SplashFragmentDirections.actionSplashFragmentToHomeFragment()
             )
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
