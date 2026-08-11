b2dffec3b0eab53663e10e1ffb8073b00063b175
pahntd <pdtrong2652000@gmail.com>
2026-08-11 11:35:07 +0700

    Implement: navigation between 4 screen

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.expensetracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.expensetracker.databinding.ActivityMainBinding
@@ -22,20 +20,30 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
+    }
 
+    private fun setupNavigation() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
 
         val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
 
-        navController.addOnDestinationChangedListener{_,destination,_->
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             binding.bottomNavigation.isVisible = destination.id in setOf(
                 R.id.homeFragment,
                 R.id.statisticsFragment,
                 R.id.categoryFragment,
                 R.id.settingsFragment
             )
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            if (navController.currentDestination?.id == item.itemId) {
+                return@setOnItemSelectedListener true
+            }
+            navController.navigate(item.itemId)
+            true
         }
     }
 }
